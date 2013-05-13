0fbe7d2973a2571962c4226f4e14bd3972959997
jlalande <jeanlalande@gmail.com>
2013-05-13 14:20:25 -0400

    Initial commit

---
+++ src/main/java/com/jlalande/example/launch4j/Main.java
@@ -0,0 +1,16 @@
+package com.jlalande.example.launch4j;
+
+/**
+ * @copyright ©2013, Bell Canada
+ * @author jean.lalande1 (CTS)
+ */
+public class Main {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
