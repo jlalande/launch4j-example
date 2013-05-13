be9132f5f9bf99899fd895da75eddf9710bfeaab
jlalande <jeanlalande@gmail.com>
2013-05-13 14:33:09 -0400

    Console now waits for user input before exiting

--- src/main/java/com/jlalande/example/launch4j/Main.java
+++ src/main/java/com/jlalande/example/launch4j/Main.java
@@ -1,9 +1,18 @@
 package com.jlalande.example.launch4j;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     System.out.println("Hello World!");
+
+    System.out.print("Press any key to continue...");
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    in.readLine();
   }
 
 }
