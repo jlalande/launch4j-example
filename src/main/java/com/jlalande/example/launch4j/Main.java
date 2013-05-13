3a66c2e85aaa5319cfd39880e1e1feb70cb6526e
jlalande <jeanlalande@gmail.com>
2013-05-13 14:23:03 -0400

    Remove invalid buildNumber based on Git SHA-1 value

--- src/main/java/com/jlalande/example/launch4j/Main.java
+++ src/main/java/com/jlalande/example/launch4j/Main.java
@@ -1,14 +1,7 @@
 package com.jlalande.example.launch4j;
 
-/**
- * @copyright ©2013, Bell Canada
- * @author jean.lalande1 (CTS)
- */
 public class Main {
 
-  /**
-   * @param args
-   */
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
